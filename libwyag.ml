1ea361c0cc73615852637361842b6d5c80bf0f59
TortueFéroce <axel.helary@gmail.com>
2025-03-26 13:06:34 +0100

    début

---
+++ libwyag.ml
@@ -0,0 +1,21 @@
+open Arg
+open Cmdliner
+open Unix
+open Filename
+open Sys
+
+type gitRepository = {
+    worktree : string;
+    gitdir : string;
+    conf : string;
+}
+
+(* la fonction is_directory du module Sys renvoie un booléen pour savoir si le string passé en argument est un dir *)
+
+let repo_path repo path =
+    (* Concatène repo et path pour avoir le chemin complet vers path *)
+    Filename.concat (repo.gitdir) path
+
+let repo_path_list repo p_list =
+    (* Concatène une liste *)
+    repo_path repo (List.fold_left (fun ongoing_path p -> Filename.concat ongoing_path p) "" p_list)
