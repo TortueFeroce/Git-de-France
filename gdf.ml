d9ab83e0de20350d74f6326dbbf3b14612a04f92
TortueFéroce <axel.helary@gmail.com>
2025-04-09 16:55:12 +0200

    types mis à jour

--- gdf.ml
+++ gdf.ml
@@ -3,17 +3,24 @@
 open Arg
 
 let print_test () = 
-  Printf.printf "coucou les copaings"
+  Printf.printf "coucou les copaings \n"
 
-let options = [("-mesgrossescouilles", Unit print_test, "ne fait rien pour l'instant")]
+let compute_init () =
+  (* Fonction qui est appelée lorsque la commande init est saisie *)
+  
+  Printf.printf "pas implémenté"
+
+let options = ref [("-mesgrossescouilles", Unit print_test, "ne fait rien pour l'instant");
+                   ("-init", Unit compute_init, "initialise un projet")]
 
 let read_option () =
   let args = ref [] in
-  Arg.parse options
+  Arg.parse_dynamic options
   (fun s -> args := s :: !args)
-  "coucou la team ça gase ?!!"
+  "Voici les commandes disponibles pour git-de-France :"
 
 let () = read_option ()
+
 
 
 (* 
--- libgdf.ml
+++ libgdf.ml
@@ -6,19 +6,9 @@ open Sys
 open Sha
 open Camlzip
 
-type gitRepository = {
-    worktree : string;
-    gitdir : string;
-    conf : string;
-}
+type path = string list
 
-(* la fonction is_directory du module Sys renvoie un booléen pour savoir si le string passé en argument est un dir *)
+type worktree = path
 
-let repo_path repo path =
-    (* Concatène repo et path pour avoir le chemin complet vers path *)
-    Filename.concat (repo.gitdir) path
-
-let repo_path_list repo p_list =
-    (* Concatène une liste *)
-    repo_path repo (List.fold_left (fun ongoing_path p -> Filename.concat ongoing_path p) "" p_list)
+type git_directory = path
 
