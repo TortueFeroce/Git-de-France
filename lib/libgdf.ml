c2338dc37f0bf745e04ddce30dc9dd6a635fa8a3
TortueFéroce <axel.helary@gmail.com>
2025-04-16 13:27:05 +0200

    error handling for already existing project

--- lib/libgdf.ml
+++ lib/libgdf.ml
@@ -4,6 +4,7 @@ open Filename
 open Sys
 open Sha1 *)
 
+exception GdfError of string
 
 type path = string list
 
@@ -14,7 +15,21 @@ type git_directory = path
 
 let perm_base = 0o777
 
+let check_init s =
+  (* Fonction qui regarde si les conditions sont remplies pour
+  initialiser un projet git *)
+  print_string s
+
+
 let compute_init s =
   (* Fonction qui est appelée lorsque la commande init est saisie *)
-  Unix.mkdir s perm_base;
-  Unix.mkdir (s^"/.git") perm_base
+  try
+    (Unix.mkdir s perm_base;
+    Unix.mkdir (s^"/.git") perm_base;
+    Unix.mkdir (s^"/.git/objects") perm_base;
+    Unix.mkdir (s^"/.git/refs") perm_base;
+    let config_descr = Unix.openfile (s^"/.git/config") [O_CREAT] perm_base in)
+  with Unix.Unix_error(_) ->
+    Unix.mkdir (s^"/.git") perm_base;
+    Unix.mkdir (s^"/.git/objects") perm_base;
+    Unix.mkdir (s^"/.git/refs") perm_base
